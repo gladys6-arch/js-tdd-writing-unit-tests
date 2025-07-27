8d63096478cb598f843b4b92ae8e722d46cd917d
gladys <achandogladys6@gmail.com>
2025-07-27 11:43:10 +0300

    add test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total pointsl, 2 per consonant)", () => {
+    const word = "test";
+
+    const points = pointsForWord(word);
+
+    expect(points).toBe(7);
+  });
+});
